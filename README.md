cfc4d2b3b8d0d120aebf4f9ab754779e2876a94e
AnaelBonnafous <71386173+AnaelBonnafous@users.noreply.github.com>
2022-12-05 20:39:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AOV2022-day4-recursive-tree
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/recursive-christmas-tree-bani5p)
---
+++ src/ChristmasTree.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <ChristmasTree v-if="size > 1" :size="size - 1" />
+
+      <div class="flex flex-row justify-center">
+        <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+</script>
